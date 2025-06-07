ec020941e97a19b03d46d6c4a62af5d143d6cb55
_.fmendes._ <fmendes_dev@protonmail.ch>
2025-06-07 21:05:21 +0100

    wip: database schema compute unix instead of timestamp

--- migrations/1749316657_initial_schema.down.sql
+++ migrations/1749316657_initial_schema.down.sql
@@ -1,5 +1,3 @@
-DROP TRIGGER IF EXISTS update_users_timestamp;
-
 DROP INDEX IF EXISTS idx_user_sessions_user_id;
 
 DROP INDEX IF EXISTS idx_users_email;
--- migrations/1749316657_initial_schema.up.sql
+++ migrations/1749316657_initial_schema.up.sql
@@ -4,16 +4,15 @@ CREATE TABLE users (
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     email_verified BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-) WITHOUT ROWID;
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000)
+);
 
 CREATE TABLE password_reset_requests (
     user_id TEXT PRIMARY KEY,
     code_challenge TEXT NOT NULL,
     code_challenge_method TEXT DEFAULT 'S256',
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
@@ -22,8 +21,8 @@ CREATE TABLE email_update_requests (
     new_email TEXT NOT NULL,
     code_challenge TEXT NOT NULL,
     code_challenge_method TEXT DEFAULT 'S256',
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
@@ -31,8 +30,8 @@ CREATE TABLE password_update_requests (
     user_id TEXT PRIMARY KEY,
     code_challenge TEXT NOT NULL,
     code_challenge_method TEXT DEFAULT 'S256',
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
@@ -40,29 +39,19 @@ CREATE TABLE email_verification_requests (
     user_id TEXT PRIMARY KEY,
     code_challenge TEXT NOT NULL,
     code_challenge_method TEXT DEFAULT 'S256',
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_sessions (
     id TEXT PRIMARY KEY,
     user_id TEXT NOT NULL,
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (CAST(strftime ('%s', 'now') AS INTEGER) * 1000),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-) WITHOUT ROWID;
+);
 
 CREATE INDEX idx_users_email ON users (email);
 
 CREATE INDEX idx_user_sessions_user_id ON user_sessions (user_id);
-
-CREATE TRIGGER update_users_timestamp AFTER
-UPDATE ON users FOR EACH ROW BEGIN
-UPDATE users
-SET
-    updated_at = CURRENT_TIMESTAMP
-WHERE
-    id = NEW.id;
-
-END;
